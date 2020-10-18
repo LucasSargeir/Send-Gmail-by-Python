08f2923c168d20c29132c43098f7460f23cb7d0c
lucassargeir <sargeirolucas@gmail.com>
2020-10-18 13:36:38 -0300

    Atualizando

--- e_mail_sender.py
+++ e_mail_sender.py
@@ -4,7 +4,6 @@ import smtplib
 from email.mime.text import MIMEText
 from decouple import config
 
-# verifica se foi passada a quatidade
 if(len(sys.argv) != 3):
 	print("Parametros inválidos")
 	print(sys.argv[0] + "<QUANTIDADE> <REMETENTE>")
@@ -19,12 +18,11 @@ print("Enviando " + str(repeat) + " e-mails...")
 print("")
 
 
-# conexão com os servidores do google
+
 smtp_ssl_host = 'smtp.gmail.com'
 smtp_ssl_port = 465
 
 
-# username ou email para logar no servidor
 username = config('PYTHON_APP_USER')
 password = config('PYTHON_APP_PASS')
 
@@ -33,25 +31,23 @@ from_addr = config('USER')
 to_addrs = [to_email]
 
 
-# usa o MIME Text para enviar somente texto
 message = MIMEText('Aqui vem a sua mensagem')
 message['subject'] = 'Assunto'
 message['from'] = from_addr
 message['to'] = ', '.join(to_addrs)
 
 
-# conecta de forma segura usando SSL
 server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
 
 
-# faz login no servidor externo
 server.login(username, password)
 
 
-# faz o envio do e-mail
 for i in range(0, repeat):
+
 	server.sendmail(from_addr, to_addrs, message.as_string())
 	print("Enviando e-mail "+ str(i + 1))
+
 	time.sleep(2)
 
 
